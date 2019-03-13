6fab0f5c82a7073f00fe350162b0afa7c4d568c7
Dmytro Orlov <orlov.dm@gmail.com>
2019-03-13 12:57:43 +0100

    IOException

--- src/main/scala/app/WeatherClient.scala
+++ src/main/scala/app/WeatherClient.scala
@@ -1,8 +1,11 @@
 package app
 
+import java.io.IOException
+
 class WeatherClient(host: String, port: Int) {
   def forecast(city: City): Forecast = city match {
     case City("Wroclaw") ⇒ Forecast(Temperature(7))
     case City("Cadiz") ⇒ Forecast(Temperature(25))
+    case _ ⇒ throw new IOException("UnknownCity")
   }
 }
