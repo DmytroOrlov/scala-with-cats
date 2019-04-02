b7654a23764964b59da72bff52e20dbdd25a4e8f
Dmytro Orlov <orlov.dm@gmail.com>
2019-04-02 17:54:54 +0200

    change weather

--- src/main/scala/app/Weather.scala
+++ src/main/scala/app/Weather.scala
@@ -1,10 +1,7 @@
 package app
 
-import app.Config._
-import cats.Id
 import cats.effect.Sync
 import cats.tagless._
-import mtl.constantAsk
 
 @finalAlg
 trait Weather[F[_]] {
@@ -13,10 +10,7 @@ trait Weather[F[_]] {
 
 object Weather {
   def weather[F[_] : Sync](config: Config): Weather[F] = new Weather[F] {
-    val client = {
-      implicit val configAsk = constantAsk[Id, Config](config)
-      new WeatherClient(host[Id], port[Id])
-    }
+    lazy val client = new WeatherClient(config.host, config.port)
 
     def forecast(city: City): F[Forecast] = Sync[F].delay(client.forecast(city))
   }
