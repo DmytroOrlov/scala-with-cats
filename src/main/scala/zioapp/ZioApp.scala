DmytroOrlov/graal-zio-forecast

add union types

src/main/scala/zioapp/ZioApp.scala
@@ -1,13 +1,63 @@
 package zioapp
 
-import app.{City, Config, Requests, Temperature, UnknownCity, WeatherClient}
+import java.io.IOException
+
+import app.{City, Config, Requests, Temperature, WeatherClient}
 import cats.implicits._
-import scalaz.zio._
-import scalaz.zio.console._
+import zio._
+import capture.Capture
 import zioapp.config._
+import zioapp.console._
+import zioapp.error.WeatherErr
 import zioapp.weather._
 
+import scala.util.matching.Regex
+import scala.reflect.runtime.universe._
+
 object ZioApp extends App {
+  def typestr[A: WeakTypeTag](x: => A): String = weakTypeOf[A].dealias.widen.toString
+
+  def askFetchJudge(reqs: Ref[Requests], client: WeatherClient) =
+    for {
+      cityName <- askCity
+      city <- cityByName(cityName)
+      f <- fetchForecast(client, city, reqs)
+      _ <- printLn(s"Forecast for $city is ${f.temperature}")
+      hottest <- hottestCity(reqs)
+      _ <- printLn(s"Hottest city found so far is $hottest")
+    } yield ()
+
+  def app(requests: Requests) = {
+    val makeLoop = for {
+      reqsRef <- Ref.make(requests)
+      host <- host
+      port <- port
+      weatherClient = new WeatherClient(host, port)
+      _ <- askFetchJudge(reqsRef, weatherClient).forever
+    } yield ()
+
+    def program = for {
+      _ <- printLn(typestr(makeLoop))
+      host <- host
+      port <- port
+      _ <- printLn(s"Using weather service at http://$host:$port")
+      _ <- makeLoop
+    } yield ()
+
+    program.foldM(
+      e ⇒ printLn(e.continue(new WeatherErr[String] with ConsoleErr[String] {
+        def weatherClient(error: IOException): String = s"weather client $error"
+
+        def unknownCity(city: String): String = s"unknown city $city"
+
+        def consoleRead(error: IOException): String = s"console read $error"
+      }))
+        .const(1),
+      _ ⇒ IO
+        .succeed(0)
+    )
+  }
+
   def hottestCity(requests: Ref[Requests]): UIO[(City, Temperature)] =
     for (reqs <- requests.get) yield Requests.hottest(reqs).map(_.temperature)
 
@@ -19,60 +69,26 @@ object ZioApp extends App {
       _ <- requests.update(_ + (city -> forecast))
     } yield forecast
 
-  def cityByName(cityName: String): IO[UnknownCity, City] = {
+  def cityByName(cityName: String): ZIO[Any, Capture[WeatherErr], City] = {
     def succeed = IO.succeed(City(cityName))
 
     cityName match {
       case "Wroclaw" ⇒ succeed
       case "Cadiz" ⇒ succeed
       case "Hamburg" ⇒ succeed
-      case _ ⇒ IO.fail(UnknownCity(cityName))
+      case _ ⇒ IO.fail(WeatherErr.unknownCity(cityName))
     }
   }
 
-  val askCity = putStrLn("What is the next city?") *> getStrLn
+  val askCity = printLn("What is the next city?") *> readLn
 
-  def askFetchJudge(reqs: Ref[Requests], client: WeatherClient) =
-    for {
-      cityName <- askCity
-      city <- cityByName(cityName)
-      f <- fetchForecast(client, city, reqs)
-      _ <- putStrLn(s"Forecast for $city is ${f.temperature}")
-      hottest <- hottestCity(reqs)
-      _ <- putStrLn(s"Hottest city found so far is $hottest")
-    } yield ()
-
-  def app(requests: Requests) = {
-    val makeLoop = for {
-      reqs <- Ref.make(requests)
-      cl <- weatherClient
-      _ <- askFetchJudge(reqs, cl).forever
-    } yield ()
-
-    val program = for {
-      h <- host
-      p <- port
-      _ <- putStrLn(s"Using weather service at http://$h:$p")
-      _ <- makeLoop
-    } yield ()
-
-    program.foldM(
-      e ⇒ putStr(s"Encountered an error: $e")
-        .const(1),
-      _ ⇒ IO
-        .succeed(0)
-    )
-  }
-
-  def run(args: List[String]) = {
+  def run(args: List[String]): UIO[Int] = {
     val conf = Config("localhost", 8080)
     val requests = Requests.empty
 
-    app(requests).provideSome[Console](c ⇒
-      new Console with Weather.Live with HasConfig {
-        val console = c.console
-
-        def apply(v1: Any) = conf
+    app(requests).provide(
+      new HasConfig with Weather.Live with Console.Live {
+        val config = conf
       })
   }
 }

src/main/scala/zioapp/error/WeatherErr.scala
@@ -1,0 +1,19 @@
+package zioapp.error
+
+import java.io.IOException
+
+import capture.Capture
+import capture.Capture.Constructors
+
+trait WeatherErr[+A] {
+  def weatherClient(error: IOException): A
+
+  def unknownCity(city: String): A
+}
+
+//this probably should be generated by some macro
+object WeatherErr extends Constructors[WeatherErr] {
+  def weatherClient(error: IOException) = Capture[WeatherErr](_.weatherClient(error))
+
+  def unknownCity(city: String) = Capture[WeatherErr](_.unknownCity(city))
+}

DmytroOrlov/scala-with-cats

src/main/scala/zioapp/ZioApp.scala
@@ -1,13 +1,63 @@
 package zioapp
 
-import app.{City, Config, Requests, Temperature, UnknownCity, WeatherClient}
+import java.io.IOException
+
+import app.{City, Config, Requests, Temperature, WeatherClient}
 import cats.implicits._
-import scalaz.zio._
-import scalaz.zio.console._
+import zio._
+import capture.Capture
 import zioapp.config._
+import zioapp.console._
+import zioapp.error.WeatherErr
 import zioapp.weather._
 
+import scala.util.matching.Regex
+import scala.reflect.runtime.universe._
+
 object ZioApp extends App {
+  def typestr[A: WeakTypeTag](x: => A): String = weakTypeOf[A].dealias.widen.toString
+
+  def askFetchJudge(reqs: Ref[Requests], client: WeatherClient) =
+    for {
+      cityName <- askCity
+      city <- cityByName(cityName)
+      f <- fetchForecast(client, city, reqs)
+      _ <- printLn(s"Forecast for $city is ${f.temperature}")
+      hottest <- hottestCity(reqs)
+      _ <- printLn(s"Hottest city found so far is $hottest")
+    } yield ()
+
+  def app(requests: Requests) = {
+    val makeLoop = for {
+      reqsRef <- Ref.make(requests)
+      host <- host
+      port <- port
+      weatherClient = new WeatherClient(host, port)
+      _ <- askFetchJudge(reqsRef, weatherClient).forever
+    } yield ()
+
+    def program = for {
+      _ <- printLn(typestr(makeLoop))
+      host <- host
+      port <- port
+      _ <- printLn(s"Using weather service at http://$host:$port")
+      _ <- makeLoop
+    } yield ()
+
+    program.foldM(
+      e ⇒ printLn(e.continue(new WeatherErr[String] with ConsoleErr[String] {
+        def weatherClient(error: IOException): String = s"weather client $error"
+
+        def unknownCity(cityName: String): String = s"unknown city $cityName"
+
+        def consoleRead(error: IOException): String = s"console read $error"
+      }))
+        .const(1),
+      _ ⇒ IO
+        .succeed(0)
+    )
+  }
+
   def hottestCity(requests: Ref[Requests]): UIO[(City, Temperature)] =
     for (reqs <- requests.get) yield Requests.hottest(reqs).map(_.temperature)
 
@@ -19,60 +69,26 @@ object ZioApp extends App {
       _ <- requests.update(_ + (city -> forecast))
     } yield forecast
 
-  def cityByName(cityName: String): IO[UnknownCity, City] = {
+  def cityByName(cityName: String): IO[Capture[WeatherErr], City] = {
     def succeed = IO.succeed(City(cityName))
 
     cityName match {
       case "Wroclaw" ⇒ succeed
       case "Cadiz" ⇒ succeed
       case "Hamburg" ⇒ succeed
-      case _ ⇒ IO.fail(UnknownCity(cityName))
+      case _ ⇒ IO.fail(WeatherErr.unknownCity(cityName))
     }
   }
 
-  val askCity = putStrLn("What is the next city?") *> getStrLn
+  val askCity = printLn("What is the next city?") *> readLn
 
-  def askFetchJudge(reqs: Ref[Requests], client: WeatherClient) =
-    for {
-      cityName <- askCity
-      city <- cityByName(cityName)
-      f <- fetchForecast(client, city, reqs)
-      _ <- putStrLn(s"Forecast for $city is ${f.temperature}")
-      hottest <- hottestCity(reqs)
-      _ <- putStrLn(s"Hottest city found so far is $hottest")
-    } yield ()
-
-  def app(requests: Requests) = {
-    val makeLoop = for {
-      reqs <- Ref.make(requests)
-      cl <- weatherClient
-      _ <- askFetchJudge(reqs, cl).forever
-    } yield ()
-
-    val program = for {
-      h <- host
-      p <- port
-      _ <- putStrLn(s"Using weather service at http://$h:$p")
-      _ <- makeLoop
-    } yield ()
-
-    program.foldM(
-      e ⇒ putStr(s"Encountered an error: $e")
-        .const(1),
-      _ ⇒ IO
-        .succeed(0)
-    )
-  }
-
-  def run(args: List[String]) = {
+  def run(args: List[String]): UIO[Int] = {
     val conf = Config("localhost", 8080)
     val requests = Requests.empty
 
-    app(requests).provideSome[Console](c ⇒
-      new Console with Weather.Live with HasConfig {
-        val console = c.console
-
-        def apply(v1: Any) = conf
+    app(requests).provide(
+      new HasConfig with Weather.Live with Console.Live {
+        val config = conf
       })
   }
 }

src/main/scala/zioapp/error/WeatherErr.scala
@@ -1,0 +1,19 @@
+package zioapp.error
+
+import java.io.IOException
+
+import capture.Capture
+import capture.Capture.Constructors
+
+trait WeatherErr[+A] {
+  def weatherClient(error: IOException): A
+
+  def unknownCity(cityName: String): A
+}
+
+//this probably should be generated by some macro
+object WeatherErr extends Constructors[WeatherErr] {
+  def weatherClient(error: IOException) = Capture[WeatherErr](_.weatherClient(error))
+
+  def unknownCity(cityName: String) = Capture[WeatherErr](_.unknownCity(cityName))
+}